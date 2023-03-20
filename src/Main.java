b0a57254182e9be72e2e1ce4d83de37c00482877
prost333 <dimabolshoy333@yandex.by>
2023-03-20 09:56:44 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+public class Main {
+    public static void main(String[] args) {
+//        task1
+//        int arr[]= new int[10];
+//        int x=0;
+//        for (int i=0;i<10;i++){
+//            arr[i]=(int)(Math.random()*100);
+//        }
+//        for (int i=0;i< arr.length;i++){
+//            if (i%2==0){
+//                arr[i]=0;
+//            }
+//        }
+//        for (int i = 0;i<arr.length;i++){
+//            System.out.println(arr[i]+" ");
+//        }
+//        task2
+//        int arr[] ={52,32,68,97,34};
+//        int max=0;
+//        int min =0;
+//        int indexmax=0;
+//        int indexmin=0;
+//        max=arr[0];
+//        for (int i =0;i<arr.length;i++){
+//            if (arr[i]>max){
+//                max=arr[i];
+//                indexmax=i;
+//            }
+//
+//        }
+//        System.out.println("max: "+max);
+//
+//        min = arr[0];
+//        for (int i = 0; i<arr.length;i++){
+//            if (min>arr[i]){
+//                min=arr[i];
+//                indexmin=i;
+//            }
+//        }
+//
+//        System.out.println("\nmin: "+min);
+//        arr[indexmax]=1;
+//        arr[indexmin]=0;
+//        for (int i = 0;i< arr.length; i++){
+//            System.out.println(arr[i]);
+//        }
+//        task 3
+//        int arr[]={5,78,9,44,12,65};
+//        int a=0;
+//        int a2=0;
+//        for (int i =0;i< arr.length;i++){
+//            for (int j=0;j<arr.length-1;j++){
+//                if (arr[j] > arr[j+1]) {
+//                    a=arr[j];
+//                    arr[j]=arr[j+1];
+//                    arr[j+1]=a;
+//                }
+//            }
+//        }
+//        for (int i =0;i<arr.length;i++){
+//            System.out.println(arr[i]);
+//        }
+//        task4
+//        int arr[]={5,78,9,44,12,65};
+//        boolean f=false;
+//
+//        while (!f){
+//            f=true;
+//        for (int j=0;j< arr.length-1;j++){
+//            if (arr[j]>arr[j+1]){
+//                int a=arr[j];
+//                arr[j]=arr[j+1];
+//                arr[j+1]=a;
+//                f=false;
+//            }
+//
+//        }
+//        }
+//        for (int i=0;i<arr.length;i++){
+//            System.out.println(arr[i]+" ");
+//        }
+    }}
+
+
